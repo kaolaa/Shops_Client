57388af3bc234eaa52a524340c889f50f483fdc1
unknown <khatrach@gmail.com>
2019-01-21 15:39:49 +0100

    test the connection with the server

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <ShopComponent/>
+  </div>
+</template>
+
+<script>
+import ShopComponent from './components/ShopComponent.vue'
+
+export default {
+  name: 'app',
+  components: {
+    ShopComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
